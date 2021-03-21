0f59fdcdbe1eff471526144c67340ebe9a1d07ff
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-03-20 18:31:53 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    const div5 = document.querySelector('div div div div div');
+
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+
+    input.addEventListener('click', clickAlert); 
+
+    div5.addEventListener('click', clickAlert);
 }
+addingEventListener();
